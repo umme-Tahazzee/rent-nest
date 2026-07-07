7b6d43866103be3de3c5dd6f9bf3a7d827b7cb59
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-07-07 14:25:43 +0600

    fear : Configuring Environment Variables With dotenv And Using Express Built In Middlwares In app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express,{ Application, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import config from "./config";
 
 const app : Application = express()
 
-
+// middleware
+app.use(
+    cors({
+        origin: config.app_url,
+        credentials: true,
+    }),
+);
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 app.get("/", (req:Request,res:Response)=>{
       res.send("hello world")
 })
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import app from "./app"
+import { prisma } from "./lib/prisma";
 
 
 const port = process.env.port || 5000
 async function main(){
       try {
+        await prisma.$connect()
+        console.log('Connect Database successfully')
         app.listen(port,()=>{
                console.log(`Server is running on ${port}`);
                
         })          
       } catch (error) {
+          
           console.log("Error starting in the server", error);
+          await prisma.$disconnect()
           process.exit(1)
       }
 }
