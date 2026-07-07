db50d9eedcdf938a548c5f1f253cd0e03708d164
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-07-07 19:43:48 +0600

    create acesstoken and refreshtoken

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express,{ Application, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import config from "./config";
+import { userRouters } from "./modules/user/user.route";
+import { authRoutes } from "./modules/auth/auth.routes";
 
 const app : Application = express()
 
@@ -18,5 +20,11 @@ app.use(cookieParser())
 app.get("/", (req:Request,res:Response)=>{
       res.send("hello world")
 })
+
+
+//routes
+app.use('/api/auth', userRouters)
+app.use('/api/auth', authRoutes)
+
 
 export default app;
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,10 @@
 import bcrypt from "bcryptjs"
 import { prisma } from "../../lib/prisma"
 import { IloginUser } from "./auth.interface"
+import jwt, { SignOptions } from "jsonwebtoken"
+import config from "../../config"
+import { jwtUtils } from "../utils/jwt"
+
 
 const loginUser = async(payload : IloginUser) =>{
 
@@ -18,7 +22,30 @@ const loginUser = async(payload : IloginUser) =>{
          throw new Error("Password is incorrect")
     }
 
-    return user
+    const jwtPayload = {
+         user : user.id,
+         name : user.name,
+         email : user.email,
+         role : user.role ,
+         status : user.status
+    }
+
+    const accessToken = jwtUtils.createToken(
+        jwtPayload, 
+        config.jwt_access_secret, 
+        config.jwt_access_expires_in as SignOptions)
+
+
+    const refreshToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_refresh_secret,
+        config.jwt_refresh_expires_in as SignOptions
+    )
+    
+
+    return {
+        accessToken, refreshToken
+    }
     
     
 }
