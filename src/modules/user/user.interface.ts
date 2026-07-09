96f0e42528d4a0ad5b024b3ff32b6bb7f346250c
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-07-09 13:38:38 +0600

    feat : user validation is set in user folder and add phone number in registraion

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -3,7 +3,8 @@ import { Role } from "../../../generated/prisma/enums"
 export interface RegisterInterfacePayload {
       name: string
       email: string
-      password: string
+      password: string,
+      phone ?: string
       role?:Role
     
 }
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { userController } from "./user.controller";
 import { auth } from "../../middleware/auth";
 import { Role } from "../../../generated/prisma/enums";
+import validateRequest from "../../middleware/validateRequest";
+import { UserValidations } from "./user.validation";
 
 const router = Router();
 
-router.post('/register',userController.createUser)
+router.post('/register',validateRequest(UserValidations.createUserValidationSchema),userController.createUser)
 router.get('/me', auth(Role.ADMIN, Role.LANDLORD, Role.TENANT), userController.getMyProfile)
 
 export const userRoutes = router
